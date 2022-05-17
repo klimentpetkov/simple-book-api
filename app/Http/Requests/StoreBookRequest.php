887bb8d8497a6522cdadf47d2046ca5cbdd839c7
Kliment_Petkov <klimopetkov@gmail.com>
2022-05-17 10:45:09 +0300

    Added validation request class

---
+++ app/Http/Requests/StoreBookRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\Author;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class StoreBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'author_id' => [
+                'required',
+                'numeric',
+                'exists:users,id',
+                new Author
+            ],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+          ], Response::HTTP_BAD_REQUEST));
+    }
+}
